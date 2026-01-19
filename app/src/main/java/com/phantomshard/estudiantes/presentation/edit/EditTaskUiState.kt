4f6b20e403d2bd5bade16e8aa7c70faa3cadfa11
Dev0x1 <byte0x1@gmail.com>
2026-01-19 11:33:15 -0400

    Crea el Edit UI State.

--- app/src/main/java/com/phantomshard/estudiantes/presentation/edit/EditTaskUiState.kt
+++ app/src/main/java/com/phantomshard/estudiantes/presentation/edit/EditTaskUiState.kt
@@ -1,4 +1,17 @@
 package com.phantomshard.estudiantes.presentation.edit
 
-class EditTaskUiState {
-}
+data class EditTaskUiState(
+    val estudianteId: Int? = null,
+    val nombre: String = "",
+    val email: String = "",
+    val edad: String = "",
+    val nombreError: String? = null,
+    val emailError: String? = null,
+    val edadError: String? = null,
+    val isSaving: Boolean = false,
+    val isDeleting: Boolean = false,
+    val isNew: Boolean = true,
+    val saved: Boolean = false,
+    val deleted: Boolean = false,
+    val errorMessage: String? = null
+)
