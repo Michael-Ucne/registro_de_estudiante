3c9e66ff78bfb82d561bd6be1c458b8da2d37b2c
Dev0x1 <byte0x1@gmail.com>
2026-01-19 12:02:58 -0400

    Crea el List UI Event.

--- app/src/main/java/com/phantomshard/estudiantes/presentation/list/ListTaskUiEvent.kt
+++ app/src/main/java/com/phantomshard/estudiantes/presentation/list/ListTaskUiEvent.kt
@@ -1,4 +1,11 @@
 package com.phantomshard.estudiantes.presentation.list
 
-interface ListTaskUiEvent {
-}   
+sealed class ListTaskUiEvent {
+    data object Load : ListTaskUiEvent()
+    data object Refresh : ListTaskUiEvent()
+    data class Delete(val id: Int) : ListTaskUiEvent()
+    data class ShowMessage(val message: String) : ListTaskUiEvent()
+    data object ClearMessage : ListTaskUiEvent()
+    data object CreateNew : ListTaskUiEvent()
+    data class Edit(val id: Int) : ListTaskUiEvent()
+}
