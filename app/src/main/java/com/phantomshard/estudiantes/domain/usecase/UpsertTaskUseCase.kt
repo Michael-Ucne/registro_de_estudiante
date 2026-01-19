03c20eab0ed4188c7b3c6f0a791f091861416c9f
Dev0x1 <byte0x1@gmail.com>
2026-01-19 10:49:24 -0400

    Crea el usecase de upsert.

--- app/src/main/java/com/phantomshard/estudiantes/domain/usecase/UpsertTaskUseCase.kt
+++ app/src/main/java/com/phantomshard/estudiantes/domain/usecase/UpsertTaskUseCase.kt
@@ -1,4 +1,28 @@
 package com.phantomshard.estudiantes.domain.usecase
 
-class UpsertTaskUseCase {
+import com.phantomshard.estudiantes.domain.model.Task
+import com.phantomshard.estudiantes.domain.repository.TaskRepository
+import javax.inject.Inject
+
+class UpsertTaskUseCase @Inject constructor(
+    private val repository: TaskRepository
+) {
+    suspend operator fun invoke(task: Task): Result<Int> {
+        val nombreResult = validateNombre(task.nombre)
+        if (!nombreResult.isValid) {
+            return Result.failure(IllegalArgumentException(nombreResult.error))
+        }
+
+        val emailResult = validateEmail(task.email)
+        if (!emailResult.isValid) {
+            return Result.failure(IllegalArgumentException(emailResult.error))
+        }
+
+        val edadResult = validateEdad(task.edad.toString())
+        if (!edadResult.isValid) {
+            return Result.failure(IllegalArgumentException(edadResult.error))
+        }
+
+        return runCatching { repository.upsert(task) }
+    }
 }
