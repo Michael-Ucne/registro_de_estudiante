63090d08b2e536755660c8d80979a4f887c3e870
Dev0x1 <byte0x1@gmail.com>
2026-01-19 10:23:10 -0400

    Crea el usecase de delete.

--- app/src/main/java/com/phantomshard/estudiantes/domain/usecase/DeleteTaskUseCase.kt
+++ app/src/main/java/com/phantomshard/estudiantes/domain/usecase/DeleteTaskUseCase.kt
@@ -1,4 +1,10 @@
 package com.phantomshard.estudiantes.domain.usecase
 
-class DeleteTaskUseCase {
+import com.phantomshard.estudiantes.domain.repository.TaskRepository
+import javax.inject.Inject
+
+class DeleteTaskUseCase @Inject constructor(
+    private val repository: TaskRepository
+) {
+    suspend operator fun invoke(id: Int) = repository.delete(id)
 }
