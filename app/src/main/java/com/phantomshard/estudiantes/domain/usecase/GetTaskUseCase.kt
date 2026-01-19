1efaee9f1aa802f688fab6e24e831f668b0b0f7d
Dev0x1 <byte0x1@gmail.com>
2026-01-19 10:32:35 -0400

    Crea el usecase de get.

--- app/src/main/java/com/phantomshard/estudiantes/domain/usecase/GetTaskUseCase.kt
+++ app/src/main/java/com/phantomshard/estudiantes/domain/usecase/GetTaskUseCase.kt
@@ -1,4 +1,11 @@
 package com.phantomshard.estudiantes.domain.usecase
 
-class GetTaskUseCase {
+import com.phantomshard.estudiantes.domain.model.Task
+import com.phantomshard.estudiantes.domain.repository.TaskRepository
+import javax.inject.Inject
+
+class GetTaskUseCase @Inject constructor(
+    private val repository: TaskRepository
+) {
+    suspend operator fun invoke(id: Int): Task? = repository.getTask(id)
 }
