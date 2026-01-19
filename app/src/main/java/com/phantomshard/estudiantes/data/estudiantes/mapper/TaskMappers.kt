ea92a7a323423758183c00b77deb5e7b36882948
Dev0x1 <byte0x1@gmail.com>
2026-01-19 10:07:04 -0400

    Crea el mappers.

--- app/src/main/java/com/phantomshard/estudiantes/data/estudiantes/mapper/TaskMappers.kt
+++ app/src/main/java/com/phantomshard/estudiantes/data/estudiantes/mapper/TaskMappers.kt
@@ -1,2 +1,18 @@
 package com.phantomshard.estudiantes.data.estudiantes.mapper
 
+import com.phantomshard.estudiantes.data.estudiantes.local.TaskEntity
+import com.phantomshard.estudiantes.domain.model.Task
+
+fun TaskEntity.toDomain(): Task = Task(
+    estudianteId = estudianteId,
+    nombre = nombre,
+    email = email,
+    edad = edad
+)
+
+fun Task.toEntity(): TaskEntity = TaskEntity(
+    estudianteId = estudianteId,
+    nombre = nombre,
+    email = email,
+    edad = edad
+)
