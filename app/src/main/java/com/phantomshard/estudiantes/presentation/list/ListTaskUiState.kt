fb5736aef113463001409be80aa24443108fff9a
Dev0x1 <byte0x1@gmail.com>
2026-01-19 12:01:02 -0400

    Crea el List UI State.

--- app/src/main/java/com/phantomshard/estudiantes/presentation/list/ListTaskUiState.kt
+++ app/src/main/java/com/phantomshard/estudiantes/presentation/list/ListTaskUiState.kt
@@ -1,4 +1,12 @@
 package com.phantomshard.estudiantes.presentation.list
 
-class ListTaskUiState {
-}
+import com.phantomshard.estudiantes.domain.model.Task
+
+data class ListTaskUiState(
+    val isLoading: Boolean = false,
+    val tasks: List<Task> = emptyList(),
+    val message: String? = null,
+    val navigateToCreate: Boolean = false,
+    val navigateToEditId: Int? = null,
+    val error: String? = null
+)
