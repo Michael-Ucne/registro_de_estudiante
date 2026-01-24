472b464a4945ae3b3a52ce1715b31b358e7fac50
Dev0x1 <byte0x1@gmail.com>
2026-01-24 16:36:58 -0400

    Agrega validacion de unicidad de nombre en UpsertAsignaturaUseCase.

---
+++ app/src/main/java/com/phantomshard/estudiantes/domain/usecase/UpsertAsignaturaUseCase.kt
@@ -0,0 +1,23 @@
+package com.phantomshard.estudiantes.domain.usecase
+
+import com.phantomshard.estudiantes.domain.model.Asignatura
+import com.phantomshard.estudiantes.domain.repository.AsignaturaRepository
+import javax.inject.Inject
+
+class UpsertAsignaturaUseCase @Inject constructor(
+    private val repository: AsignaturaRepository
+) {
+    suspend operator fun invoke(asignatura: Asignatura): Result<Unit> {
+        if (validateCodigo(asignatura.codigo).isValid.not()) return Result.failure(Exception("Código inválido"))
+        if (validateNombreAsignatura(asignatura.nombre).isValid.not()) return Result.failure(Exception("Nombre inválido"))
+        if (validateAula(asignatura.aula).isValid.not()) return Result.failure(Exception("Aula inválida"))
+        if (validateCreditos(asignatura.creditos.toString()).isValid.not()) return Result.failure(Exception("Créditos inválidos"))
+        
+        if (repository.existsByNombre(asignatura.nombre, asignatura.asignaturaId)) {
+            return Result.failure(Exception("Ya existe una asignatura registrada con este nombre"))
+        }
+
+        repository.upsert(asignatura)
+        return Result.success(Unit)
+    }
+}
